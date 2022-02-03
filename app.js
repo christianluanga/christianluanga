fd354001fb9cff4c5877ff8a079508838444eade
Christian Munyika <christianluanga@gmail.com>
2022-02-03 15:31:33 +0000

    commit 1

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) =>{
+	res.send("Hello there. from your Nginx server")
+})
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
